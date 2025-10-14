97aebef42088a2acecc2a953896d73e05a3a6e6d
Mike <mkburns61@yahoo.com>
2025-10-14 17:33:09 -0400

    Added files.

--- .config/emacs-common/build-emacs-env.sh
+++ .config/emacs-common/build-emacs-env.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
+
+# This a basic install script for my emacs environment. It is not
+# intended to be fully automated but to do enought installation
+# and have enough documented that any issues can be easily resolved.
+#
+# Post script:
+# 1) Install mu from aur
+# 2) install feh
+# 3) Make sure to fix .bashrc or .zshrc below
+# 4) Restore Maildir
+# 5) Restore .mbsyncrc
+# 6) Install msmtp isync (sudo pacman -S msmtp isync)
+# 7) Restore ~/.config/msmtp/*
+# 8) Rebuild mail index:
+# 8.1) mu init --maildir=~/Maildir --my-address=mkburns61@yahoo.com
+# 8.2) mu index
+##################################################################
+# Modify .zshrc for remote connections to access this device
+# Early exit for ssh connections using emacs TRAMP
+# if [[ "$TERM" == "dumb" && -n "$SSH_CONNECTION" ]]; then
+#    exec /bin/bash --noprofile --norc
+#    return
+# fi
+##################################################################
+
 set -euo pipefail
 
 echo "==> Creating required directories..."
@@ -6,7 +31,8 @@ mkdir -p \
   ~/.config/emacs-common \
   ~/.config/systemd/user \
   ~/.config/emacs-prod/modules \
-  ~/.local/bin
+  ~/.config/msmtp \
+  ~/.local/bin 
 
 echo "==> Cloning emacs_babel_config..."
 git clone https://github.com/oldfart-maker/emacs_babel_config.git /tmp/emacs_babel_config
@@ -29,9 +55,12 @@ git -C /tmp/lenovo-dotfiles checkout
 
 echo "==> Copying emacs-common files..."
 cp /tmp/lenovo-dotfiles/.config/emacs-common/emacs-prod.service ~/.config/systemd/user/
-sudo cp /tmp/lenovo-dotfiles/.config/emacs-common/emacs-prod.desktop /usr/share/applications/
 cp /tmp/lenovo-dotfiles/.config/emacs-common/emacs-profile ~/.local/bin/
 cp /tmp/lenovo-dotfiles/.config/emacs-common/emacsr ~/.local/bin/
+cp /tmp/lenovo-dotfiles/.config/emacs-common/init.el ~/.config/emacs-prod
+cp /tmp/lenovo-dotfiles/.config/emacs-common/api-keys.el ~/.config/emacs-common
+cp /tmp/lenovo-dotfiles/.config/emacs-common/bbdb ~/.config/emacs-common
+sudo cp /tmp/lenovo-dotfiles/.config/emacs-common/emacs-prod.desktop /usr/share/applications/
 
 echo "==> Registering and starting emacs-prod.service..."
 systemctl --user daemon-reexec
